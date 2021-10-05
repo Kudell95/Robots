686c47114ea6c464e6d7a934246401a25fd5bcdb
Thormy <kudell95@hotmail.com>
2021-10-05 12:25:40 +1000

    initial commit

---
+++ Robots/Classes/Board.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots.Classes
+{
+    public class Board
+    {
+        Robot ActiveRobot;
+        List<Robot> Robots = new List<Robot>();
+
+        public BoardSpace[,] spaces;
+
+        public void Init()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    spaces[i, j] = new BoardSpace();
+                }
+            }
+        }
+
+    }
+}
---
+++ Robots/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robots.Classes;
+
+ class Program
+ {
+        
+     static void Main(string[] args)
+     {
+        //Initialise the board, will contain the list of available boards
+        Board board = new Board();
+        board.Init();
+
+
+     }
+ }
